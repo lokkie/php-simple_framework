670b87f8aa8f5a2b84ff4571c7ef058f459e4be2
Alex D'Ark <lokisar@gmail.com>
2014-07-14 13:54:09 +0000

    Autoloader &&  others

--- source/index.php
+++ source/index.php
@@ -4,3 +4,12 @@
  * Date: 12.02.14
  * Time: 13:42
  */ 
+ 
+ 
+ 
+ 
+# load path constants && autoloader
+include_once dirname(__FILE__) . DIRECTORY_SEPARATOR . "path_constants.php"; 
+require_once implode(DIRECTORY_SEPARATOR , [dirname(__FILE__), 'core', 'autoLoad', 'AutoLoader.php']);
+\core\autoLoad\AutoLoader::addCodeRoute('Basic');
+
