6a83766b309f1e14a6a315aa414fed8417bfb96b
Alex D'Ark <lokisar@gmail.com>
2014-11-07 20:45:59 +0000

    UnitTest:4

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,5 +10,6 @@ spl_autoload_register('loader');
 
 # including auto-loading system 
 require_once implode(DIRECTORY_SEPARATOR , [dirname(dirname(__FILE__)), 'source', 'core', 'autoLoad', 'AutoLoader.php']);
+require_once implode(DIRECTORY_SEPARATOR , [dirname(dirname(__FILE__)), 'source', 'core', 'autoLoad', 'BasicAutoLoader.php']);
 # enabling basic class route
 \core\autoLoad\AutoLoader::addCodeRoute('Basic');
