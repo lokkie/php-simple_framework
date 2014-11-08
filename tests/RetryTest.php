cb383c3619e8f98cf62aaccd6f5f774a8b110452
Alex D'Ark <lokisar@gmail.com>
2014-11-08 12:07:05 +0000

    RetryTesting:4

--- tests/RetryTest.php
+++ tests/RetryTest.php
@@ -4,15 +4,15 @@ class RetryTest extends PHPUnit_Framework_TestCase {
 
 	public function testNoDelayRetry() {
 		$counter = 0;
-		$x = function (&$c) {
-			$c++;
+		$x = function () use ($counter) {
+			$counter++;
 			throw new \Exception("Yahoo");
 		};
 		
 		try {
 			\core\utils\Retrying::retry($x, [], ['\Exception'], 3, 0, 0);
 		} catch (\Exception $error) {
-			$this->assertEquals(3, $counter);
+			$this->assertEquals(4, $counter);
 		}
 	}
 }
