766b585940aaf9d0568897de9c43f317673f43e6
Alex D'Ark <lokisar@gmail.com>
2014-11-08 15:26:28 +0000

    RetryTests

--- tests/RetryTest.php
+++ tests/RetryTest.php
@@ -10,24 +10,28 @@ class RetryTest extends PHPUnit_Framework_TestCase {
 	}
 
 	public function testNoDelayRetry() {
+		var_dump('runWithException test');
 		self::$exceptionProvider->reset();
 		try {
 			\core\utils\Retrying::retry([self::$exceptionProvider, 'runWithException'], [1], ['\Exception'], 3, 0, 0);
 		} catch (\Exception $error) {
 			$this->assertEquals(3, self::$exceptionProvider->getCounter());
 		}
+		var_dump('runWithException incorrect Exception waiting test');
 		self::$exceptionProvider->reset();
 		try {
 			\core\utils\Retrying::retry([self::$exceptionProvider, 'runWithException'], [1], ['\RuntimeException'], 3, 0, 0);
 		} catch (\Exception $error) {
 			$this->assertEquals(1, self::$exceptionProvider->getCounter());
 		}
+		var_dump('runWithRuntimeException test');
 		self::$exceptionProvider->reset();
 		try {
 			\core\utils\Retrying::retry([self::$exceptionProvider, 'runWithRuntimeException'], [1], ['\RuntimeException'], 3, 0, 0);
 		} catch (\Exception $error) {
 			$this->assertEquals(3, self::$exceptionProvider->getCounter());
 		}
+		var_dump('runWithNoException test');
 		self::$exceptionProvider->reset();
 		try {
 			\core\utils\Retrying::retry([self::$exceptionProvider, 'runWithNoException'], [1], ['\RuntimeException'], 3, 0, 0);
